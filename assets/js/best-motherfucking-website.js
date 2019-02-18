9a6638ba2306b1f24b4860ed30c26a52730cb0d6
Thom Bruce <thom@thombruce.com>
2019-02-18 22:12:17 +0000

    cause invMode and contrast to persist between pages and visits

--- assets/js/best-motherfucking-website.js
+++ assets/js/best-motherfucking-website.js
@@ -5,6 +5,11 @@ var documentElement = document.getElementsByTagName("html")[0],
   classMethods = ["remove", "add"];
 
 function someControl(id, textArr, className) {
+  checkCookie(
+    id,
+    textArr,
+    className
+  );
   /* You see? No fucking jQuery needed, check:
    * http://www.vanilla-js.com/
    * http://jsperf.com/getelementbyid-vs-jquery-id/44
@@ -12,10 +17,12 @@ function someControl(id, textArr, className) {
   var el = document.getElementsByTagName("html")[0];
   var acbox = document.getElementById(id),
     textNode = acbox.firstChild,
-    toggled = false;
+    toggled = getCookie(id) === "true";
   acbox.addEventListener(
     clickEvent,
     function() {
+      setCookie(id, !toggled, 365);
+      // TODO: Below used here and in checkCookie; ideally, it can be refactored.
       var selector = Number((toggled = !toggled));
       textNode.data = textArr[selector];
       el.classList[classMethods[selector]](className);
@@ -42,3 +49,39 @@ function addInvertedControl() {
 
 addContrastControl();
 addInvertedControl();
+
+// HugoModo
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie(cname, textArr, className) {
+  var status = getCookie(cname);
+  if (getCookie(cname) === "true") {
+    var el = document.getElementsByTagName("html")[0];
+    var acbox = document.getElementById(cname),
+      textNode = acbox.firstChild
+
+    textNode.data = textArr[1];
+    el.classList[classMethods[1]](className);
+  }
+}
